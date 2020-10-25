546949ed5d365d0f9891bd7e03abeae98ff2bb4c
pmodv <pvernes@gmail.com>
2020-10-25 16:14:31 -0400

    Update rejection_gibbs.py
    added marginals

--- rejection_gibbs.py
+++ rejection_gibbs.py
@@ -46,3 +46,68 @@ plt.plot(x,(1/40)*(2*x +3))
 
 plt.show()
 print(count)
+
+# rejection sampling with marginals
+
+# rejection sampling example
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+
+# f(x) = (2x+3y+2)
+
+# support is [[0,2],[0,2]]
+
+# we will sample a z~U(0,2)
+# then, we get g(z), which, in our case, is uniform 0.5
+# however, this will not envelope our f(x) (peak of 12), and so we choose m = 25, so it envelopes f(x)
+
+# for each iteration, sample a z~U(0,2);
+# then, sample a u~U(0,1);
+# if  g(z) = (0.5*25*u) < f(z), then keep z; otherwise, don't keep("reject") z
+
+x = np.ones(2000, dtype=object)*-1
+y = np.ones(2000, dtype=object)*-1
+
+print(x)
+for i in range(1,2000):
+         #sample from x | y, using rejection sampling
+    z = 0
+    while (z==0):
+        u = np.random.rand()*2
+        if (((2*u)+3*y[i-1] + 2) > (25*np.random.rand()*.5)):
+            x[i]=u
+            z=1
+            
+ 
+       # sample from y | x, using rejection sampling
+    z = 0
+    while (z==0):
+        u = np.random.rand()*2
+        if (((2*x[i])+3*u + 2) > (25*np.random.rand()*.5)):
+    
+            y[i]=u
+            z=1
+    
+ 
+
+print(np.ones_like(x),len(x))
+
+plt.hist(x, normed=True,facecolor='green', alpha=0.75)
+
+plt.plot(x, (1/28)*(4.0*x+10.0))
+
+plt.show()
+
+plt.hist(y, normed=True,facecolor='yellow', alpha=0.75)
+
+plt.plot(y,  (1/28)*(6*y+8))
+
+
+
+plt.show()
+
+
+
